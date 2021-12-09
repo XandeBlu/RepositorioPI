9bd2e79bd9248116a012d7f28b73cd96cfb16987
XandeBlu <alexandrezaninifilho@gmail.com>
2021-12-09 20:04:05 -0300

    Organizando

--- CodigodeConexaoemPHP.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "estudozanini15";
-$dbname = "hannakupas";
-
-//cria a conexão
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-?>
