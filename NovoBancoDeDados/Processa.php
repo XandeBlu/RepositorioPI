33d2bc6a79aff38fbfe1dfc158e9e6483a3f17b2
XandeBlu <alexandrezaninifilho@gmail.com>
2021-12-14 19:54:49 -0300

    Recoloquei o PaginaRegistroHTML na pasta de bd

--- NovoBancoDeDados/Processa.php
+++ NovoBancoDeDados/Processa.php
@@ -13,8 +13,8 @@ $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 if(mysqli_insert_id($conn)){
 	$_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso</p>";
-	header("Location: index.html");
+	header("Location: index.php");
 }else{
 	$_SESSION['msg'] = "<p style='color:red;'>Usuário não foi cadastrado com sucesso</p>";
-	header("Location: index.html");
+	header("Location: index.php");
 }
