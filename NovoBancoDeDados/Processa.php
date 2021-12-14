3e1d27724432a21e7e1db0b6f702b73201bd4619
XandeBlu <alexandrezaninifilho@gmail.com>
2021-12-14 20:20:17 -0300

    AAAAAAAA TO FICANDO LOUCO

--- NovoBancoDeDados/Processa.php
+++ NovoBancoDeDados/Processa.php
@@ -1,14 +1,16 @@
 <?php
-
+session_start();
 include_once("conexao.php");
 
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$sobrenome = filter_input(INPUT_POST, 'sobrenome', FILTER_SANITIZE_EMAIL);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_String);
+$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_String);
 
 //echo "Nome: $nome <br>";
 //echo "E-mail: $email <br>";
 
-$result_usuario = "INSERT INTO clientes (nome, email) VALUES ('$nome', '$email')";
+$result_usuario = "INSERT INTO clientes (email, nome, sobrenome ,senha) VALUES ( '$email', '$nome', '$sobrenome', $senha)";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 if(mysqli_insert_id($conn)){
--- NovoBancoDeDados/index.php
+++ NovoBancoDeDados/index.php
@@ -1,20 +1,22 @@
-
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
 		<title>CRUD - Cadastrar</title>		
 		
+
+	</head>
+	<body>
+		<h1>Cadastrar Usuário</h1>
 		<?php
 if(isset($_SESSION['msg'])){
     echo $_SESSION['msg'];
     unset($_SESSION['msg']);
 }
 ?>
-	</head>
-	<body>
-		<h1>Cadastrar Usuário</h1>
-
 		<form method="POST" action="processa.php">
 			<label>Nome: </label>
 			<input type="text" name="nome" placeholder="Digite o nome completo"><br><br>
