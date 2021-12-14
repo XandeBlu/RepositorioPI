7f5b73962ff495a4033424dd0a6ae25aea21048e
XandeBlu <alexandrezaninifilho@gmail.com>
2021-12-14 16:35:00 -0300

    mudanças

--- NovoBancoDeDados/Conexao.php
+++ NovoBancoDeDados/Conexao.php
@@ -6,3 +6,7 @@ $dbname = "hannakupas";
 
 //Criar a conexao
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+if(isset($_SESSION['msg'])){
+    echo $_SESSION['msg'];
+    unset($_SESSION['msg']);
+}
--- NovoBancoDeDados/Processa.php
+++ NovoBancoDeDados/Processa.php
@@ -1,22 +1,20 @@
 <?php
 session_start();
-include_once("Conexao.php");
+include_once("conexao.php");
 
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-$Sobrenome = filter_input(INPUT_POST, 'sobrenome', FILTER_SANITIZE_STRING);
-$Senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 
 //echo "Nome: $nome <br>";
 //echo "E-mail: $email <br>";
 
-$result_usuario = "INSERT INTO clientes (nome, sobrenome, senha, email) VALUES ('$nome','$Sobrenome','$Senha','$email')";
+$result_usuario = "INSERT INTO clientes (nome, email) VALUES ('$nome', '$email')";
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
 if(mysqli_insert_id($conn)){
 	$_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso</p>";
-	header("Location: PaginadeRegistroHTML.html");
+	header("Location: conexao.php");
 }else{
 	$_SESSION['msg'] = "<p style='color:red;'>Usuário não foi cadastrado com sucesso</p>";
-	header("Location: PaginadeRegistroHTML.html");
+	header("Location: conexao.php");
 }
