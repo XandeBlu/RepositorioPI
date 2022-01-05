88078a7cce8205cd20b387c4c2d03eecfbfd1b21
XandeBlu <alexandrezaninifilho@gmail.com>
2022-01-05 20:03:58 -0300

    Arquivo de Conexão

---
+++ Hanna Kuppas/Conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "estudozanini15";
+$dbname = "hannakupas";
+
+//Criar A Conexão
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
