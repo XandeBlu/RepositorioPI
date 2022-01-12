cc3af9d2404aef7d50dd1067bba2144f722ec22f
SazzaSan <95775320+SazzaSan@users.noreply.github.com>
2022-01-12 13:36:17 -0300

    Mudanças

--- Hanna Kuppas/index.php
+++ Hanna Kuppas/index.php
@@ -2,12 +2,13 @@
     
     <head>
         <!-- tittle>Hanna Kuppas</tittle -->
-        <link href="../Hanna Kuppas/css/style.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
         <meta name="viewport" content="with=device-width, initial-scale=1.0, maximum-scale=1.0">
         <title>Home</title>
     </head>
 
     <body>
+
         <header>
             <div class="center">
                 <div class="logo"><img src=""></div>
@@ -15,8 +16,7 @@
                     <a href="index.php">Home</a>
                     <a href="pagProcedimentos.html">Procedimentos</a>
                     <a href="pagAgenda.php">Agende</a>
-                    <a href="C:\wamp64\www\UltimoVirtualHost\Hanna Kuppas\pagLogin.php">Login</a>
-                    <!-- a href="../Hanna Kuppas/pagRegistro.html"> Cadastro</a -->
+                    <a href="pagLogin.php">Login</a>
                     <a href="pagSair.php">Sair</a>
                 </div>
             </div>
@@ -25,22 +25,65 @@
         <section class="sobre">
 
             <div class="extras">
-                <!-- Div do Social Media-->
+
             </div>
 
             <div class="icone">
-                <!-- img src="img/Icone.jpg" -->
+                <img src="ImagensSite/Icone.jpg" style="border-radius: 50%;">
             </div>
 
             <div class="center">
                 <div class="texto-sobre">
                     <h1 style="color: #323232;">Seja Bem Vindo<br>A Agenda Digital<br><span style="color: indianred">HK Estética</span></h1>
-                    <!-- p>A estética Hanna Kupas oferece diversos procedimentos das mais variadas formas, sendo mais de dezoito tipos diferentes de técnicas. Alguns exemplos são a termoterapia, a drenagem linfática, o shiatsu e até mesmo a intradermoterapia pressurizada.<br><br>Além disso, o estabelecimento também oferece atendimento de nutricionismo, oferecendo desde consultas avulsas a pacotes mensais.</p -->
-                    <!-- button>Agende</button -->
+                    <p>A estética Hanna Kupas oferece diversos procedimentos das mais variadas formas, sendo mais de dezoito tipos diferentes de técnicas.<br><br>Alguns exemplos são a termoterapia, a drenagem linfática, o shiatsu e até mesmo a intradermoterapia pressurizada.<br><br>Além disso, o estabelecimento também oferece atendimento de nutricionismo, oferecendo desde consultas avulsas a pacotes mensais.</p>
                 </div>
             </div>
-        </section>
-    </body>
 
+        </section>
+
+        <div class="content">
+
+            <div class="slides">
+                <input type="radio" name="slide" id="slide1">
+                <input type="radio" name="slide" id="slide2">
+                <input type="radio" name="slide" id="slide3">
+                <input type="radio" name="slide" id="slide4">
+                <input type="radio" name="slide" id="slide5">
+
+                <div class="slide s1">
+                    <img src="ImagensSite/Balcao.jpeg" alt="">
+                </div>
+
+                <div class="slide">
+                    <img src="ImagensSite/EstanteDois.jpeg" alt="">
+                </div>
+
+                <div class="slide">
+                    <img src="ImagensSite/Quadros.jpeg" alt="">
+                </div>
+
+                <div class="slide">
+                    <img src="ImagensSite/Produto.jpeg" alt="">
+                </div>
+
+                <div class="slide">
+                    <img src="ImagensSite/ProdutoDois.jpeg" alt="">
+                </div>
+
+            </div>
+
+            <div class="navigation">
+
+                <label class="bar" for="slide1"></label>
+                <label class="bar" for="slide2"></label>
+                <label class="bar" for="slide3"></label>
+                <label class="bar" for="slide4"></label>
+                <label class="bar" for="slide5"></label>
+                
+            </div>
+
+        </div>
+
+    </body>
 
 </html>
